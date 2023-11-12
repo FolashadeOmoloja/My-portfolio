33980c29548ef70a2a4f053b7c51a40ef7c1242b
FolashadeOmoloja <omolojashade@gmail.com>
2023-11-12 20:43:01 +0100

    changes

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,19 +1,22 @@
 import React, { useEffect } from 'react'
-// import AOS from 'aos';
-// import 'aos/dist/aos.css'; 
 import './about.css'
 import My_Image from '../../assets/Me.png'
+import { motion } from 'framer-motion'
+import { fadeIn } from '../../variants'
 
 
 const About = () => {
-   // useEffect(() =>{
-   //    AOS.init({ duration: 1000});
-   // }, [])
+
   return (
     <section id='about' className='section'>
       <h5>Get to know</h5>
       <h2>About Me</h2>
-      <div className="container about__container" data-aos="fade-down">
+      <motion.div className="container about__container" 
+         variants={fadeIn('down', 0.2)}
+         initial='hidden'
+         whileInView={'show'}
+         viewport={{once: false,amount:0.2}}    
+      >
       <div className="about__me">
          <img src= {My_Image} alt="" className="my__image" />
       </div>
@@ -25,7 +28,7 @@ const About = () => {
             team player ready to contribute to your organization’s success.
          </p>
       </div>
-      </div>
+      </motion.div>
       </section>
   )
 }
--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,154 +1,96 @@
-import React, { useEffect } from 'react'
-import AOS from 'aos';
-import 'aos/dist/aos.css'; 
-import './portfolio.css'
+import React from 'react';
+import { GiBinoculars } from 'react-icons/gi';
+import { BsGithub, BsLink45Deg } from 'react-icons/bs';
+import { motion } from 'framer-motion'
+import { fadeIn } from '../../variants'
+import './portfolio.css';
 import img1 from '../../assets/notelify.png'
 import img2 from '../../assets/Readym.PNG'
 import img3 from '../../assets/fastkar.png'
 import img4 from '../../assets/quickpay.png'
 import img5 from '../../assets/game.png'
-import img6 from '../../assets/signup.png'
-import {GiBinoculars} from 'react-icons/gi'
 import img7 from '../../assets/planet.PNG'
-import {BsGithub} from 'react-icons/bs'
-import {BsLink45Deg} from 'react-icons/bs'
-import {FaGrinWink} from 'react-icons/fa'
+
+
+const projects = [
+  {
+    title: 'Quickpay',
+    image: img4,
+    githubLink: 'https://github.com/FolashadeOmoloja/Quick-Pay',
+    liveDemoLink: 'https://quick-pay-beta.vercel.app/',
+    description: 'Landing page for a payment platform Quickpay, for seamless payment of bills at convenience. Tech stack: Next js, typescript, framer-motion.'
+  },
+  {
+    title: 'Fastkar',
+    image: img3,
+    githubLink: 'https://github.com/FolashadeOmoloja/FastKar',
+    liveDemoLink: 'https://fast-kar.vercel.app',
+    description: 'FastKar ensures seamless, efficient transportation services for convenience and peace of mind. Tech stack: Next js, Next-auth, tailwind css, leaftlet-map, typescript.'
+  },
+  {
+    title: 'Planet Protectors',
+    image: img7,
+    githubLink: 'https://github.com/FolashadeOmoloja/Planet-Protectors',
+    liveDemoLink: 'https://planetprotectors.netlify.app/home',
+    description: 'A responsive web application built with React.js, Bootstrap, and React Table for a user-friendly interface and enhance functionality. It addresses the problem of plastic recycling.'
+  },
+  {
+    title: 'Readym',
+    image: img2,
+    githubLink: 'https://github.com/FolashadeOmoloja/Readym',
+    liveDemoLink: 'https://readym.netlify.app/',
+    description: 'Readym is a website built to improve reading culture by creating easy accessibility to informative books online, Technology used: Html, CSS and Bootstrap.'
+  },
+  {
+    title: 'Notelify',
+    image: img1,
+    githubLink: 'https://github.com/FolashadeOmoloja/Notelify',
+    liveDemoLink: 'https://notelifyweb.netlify.app/',
+    description: 'Notefily is a responsive functional web application built to help users store notes and create todo-lists, Tech stack: Html, CSS, Javascript.'
+  },
+  {
+    title: 'RPS-gameplay',
+    image: img5,
+    githubLink: 'https://github.com/FolashadeOmoloja/rock_paper_scissors_game',
+    liveDemoLink: 'https://rps-gameplay.netlify.app',
+    description: 'RPS-gameplay is a simple rock-paper-scissors game, built for recreational purposes and to let off steam <FaGrinWink/>! Tech stack: Html, CSS, and Javascript.'
+  },
+];
 
 const Portfolio = () => {
-  useEffect(() =>{
-    AOS.init({  duration: 1000 });
-    AOS.init({  disable: 'mobile'});
- }, [])
+
+
   return (
     <section id='portfolio'>
       <h5>Recent Projects</h5>
       <h2>My Portfolio</h2>
-      <div className="portfolio__container" data-aos="fade-down">
-        <article className='portfolio__item'>
-          <div className="portfolio__img">
-            <img src={img4} alt="" />
-            <div className="overlay"></div>
-								<a href="https://quick-pay-beta.vercel.app/" className="overlay__link">
-                  <GiBinoculars/>
-								</a>
-          </div>
-          <h3>Quickpay</h3>
-          <div className='portfolio__buttons'>
-          <a href="https://github.com/FolashadeOmoloja/Quick-Pay" className="btn btn-1 btn-6 btn-6a" target="_blank"><span>Github</span>  <BsGithub/></a>
-          <a href="https://quick-pay-beta.vercel.app/" className="btn btn-2 btn-6 btn-6b" target="_blank"><span>Live Demo </span><BsLink45Deg/></a>
-          </div>
-          <p>Landing page for a payment platform Quickpay, for seamless payment of bills at convenience. Tech stack: Next js, typescript, framer-motion.</p>
-        </article>
-        <article className='portfolio__item middle' data-aos="fade-down">
-          <div className="portfolio__img">
-            <img src={img3} alt="" />
-            <div className="overlay"></div>
-								<a href="https://fast-kar.vercel.app" className="overlay__link">
-                  <GiBinoculars/>
-								</a>
-          </div>
-          <h3>Fastkar</h3>
-          <div className='portfolio__buttons'>
-          <a href="https://github.com/FolashadeOmoloja/FastKar" className="btn btn-1 btn-6 btn-6a" target="_blank"><span>Github</span>  <BsGithub/></a>
-          <a href="https://fast-kar.vercel.app" className="btn btn-2 btn-6 btn-6b" target="_blank"><span>Live Demo </span><BsLink45Deg/></a>
-          </div>
-          <p>FastKar ensures seamless, efficient transportation services for convenience and peace of mind. Tech stack: Next js, Next-auth, tailwind css, leaftlet-map, typescript.</p>
-        </article>
-        <article className='portfolio__item' data-aos="fade-down">
-          <div className="portfolio__img">
-            <img src={img7} alt="" />
-            <div className="overlay"></div>
-								<a href="https://planetprotectors.netlify.app/home" className="overlay__link">
-                  <GiBinoculars/>
-								</a>
-          </div>
-          <h3>Planet Protectors</h3>
-          <div className='portfolio__buttons'>
-          <a href="https://github.com/FolashadeOmoloja/Planet-Protectors" className="btn btn-1 btn-6 btn-6a" target="_blank"><span>Github</span>  <BsGithub/></a>
-          <a href="https://planetprotectors.netlify.app/home" className="btn btn-2 btn-6 btn-6b" target="_blank"><span>Live Demo </span><BsLink45Deg/></a>
-          </div>
-          <p>A responsive web application built with React.js, Bootstrap, and React Table for a user-friendly interface and enhance functionality. It addresses the problem of plastic recycling.</p>
-        </article>
-        <article className='portfolio__item ' data-aos="fade-down">
-          <div className="portfolio__img">
-            <img src={img2} alt="" />
-            <div className="overlay"></div>
-								<a href="https://readym.netlify.app/" className="overlay__link">
-                  <GiBinoculars/>
-								</a>
-          </div>
-          <h3>Readym</h3>
-          <div className='portfolio__buttons'>
-           <a href="https://github.com/FolashadeOmoloja/Readym" className="btn btn-1 btn-6 btn-6a" target="_blank"><span>Github</span>  <BsGithub/></a>
-           <a href="https://readym.netlify.app/" className="btn btn-2 btn-6 btn-6b" target="_blank"><span>Live Demo </span><BsLink45Deg/></a>
-          </div>
-          <p>Readym is a website built to improve reading culture by creating easy accessibility to informative books online, Tecnology used: Html, CSS and Bootsrap  </p>
-        </article>
-        <article className='portfolio__item middle'>
-          <div className="portfolio__img">
-            <img src={img1} alt="" />
-            <div className="overlay"></div>
-								<a href="https://notelifyweb.netlify.app/" className="overlay__link">
-                  <GiBinoculars/>
-								</a>
-          </div>
-          <h3>Notelify</h3>
-          <div className='portfolio__buttons'>
-          <a href="https://github.com/FolashadeOmoloja/Notelify" className="btn btn-1 btn-6 btn-6a" target="_blank"><span>Github</span>  <BsGithub/></a>
-          <a href="https://notelifyweb.netlify.app/" className="btn btn-2 btn-6 btn-6b" target="_blank"><span>Live Demo </span><BsLink45Deg/></a>
-          </div>
-          <p>Notefily is a responsive functional web application built to help users store notes and create todo-lists, Tech stack: Html, CSS, Javascript</p>
-        </article>
-
-        <article className='portfolio__item' data-aos="fade-down">
-          <div className="portfolio__img">
-            <img src={img5} alt="" />
-            <div className="overlay"></div>
-								<a href="https://rps-gameplay.netlify.app" className="overlay__link">
-                  <GiBinoculars/>
-								</a>
-          </div>
-          <h3>RPS-gameplay</h3>
-          <div className='portfolio__buttons'>
-          <a href="https://github.com/FolashadeOmoloja/rock_paper_scissors_game" className="btn btn-1 btn-6 btn-6a" target="_blank"><span>Github</span>  <BsGithub/></a>
-          <a href="https://rps-gameplay.netlify.app" className="btn btn-2 btn-6 btn-6b" target="_blank"><span>Live Demo </span><BsLink45Deg/></a>
-          </div>
-          <p>RPS-gameplay is a simple rock-paper-scissors game, built for recreational purposes and to Let off steam  <FaGrinWink/>! Tech stack Html, CSS and Javascript</p>
-        </article>
-        {/* <article className='portfolio__item' data-aos="fade-down">
-          <div className="portfolio__img">
-            <img src={img6} alt="" />
-            <div className="overlay"></div>
-								<a href="https://actionsignuppage.netlify.app/" className="overlay__link">
-                  <GiBinoculars/>
-								</a>
-          </div>
-          <h3>SignUp Page</h3>
-          <div className='portfolio__buttons'>
-          <a href="https://github.com/FolashadeOmoloja/1st-mini-project-WTF--signup-form" className="btn btn-1 btn-6 btn-6a" target="_blank"><span>Github</span>  <BsGithub/></a>
-          <a href="https://actionsignuppage.netlify.app/" className="btn btn-2 btn-6 btn-6b" target="_blank"><span>Live Demo </span><BsLink45Deg/></a>
-          </div>
-          <p>This is a simple  reponsive Sign-Up web page, skillfully designed and developed to be visually appealing and attractive to users. Tech stack Html and CSS</p>
-        </article> */}
-                {/* <article className='portfolio__item' data-aos="fade-down">
-          <div className="portfolio__img">
-            <img src={img4} alt="" />
-            <div className="overlay"></div>
-								<a href="https://eduford-learning.netlify.app/" className="overlay__link">
-                  <GiBinoculars/>
-								</a>
-          </div>
-          <h3>Eduford</h3>
-          <div className='portfolio__buttons'>
-          <a href="https://github.com/FolashadeOmoloja/Eduford" className="btn btn-1 btn-6 btn-6a" target="_blank"><span>Github</span>  <BsGithub/></a>
-          <a href="https://eduford-learning.netlify.app/" className="btn btn-2 btn-6 btn-6b" target="_blank"><span>Live Demo </span><BsLink45Deg/></a>
-          </div>
-          <p>Eduford is a responsive website created as a landing page for a university containing vital imformation about the institute, Tech stack: Html, CSS, Javascript </p>
-        </article> */}
-
-      </div>
+      <motion.div 
+         variants={fadeIn('down', 0.8)}
+         initial='hidden'
+         whileInView={'show'}
+         viewport={{once: false,amount:0.2}}    
+      className="portfolio__container" >
+        {projects.map((project, index) => (
+          <article           
+          className={`portfolio__item ${index % 3 === 1 && 'middle'}`} key={index}>
+            <div className="portfolio__img">
+              <img src={project.image} alt="" />
+              <div className="overlay"></div>
+              <a href={project.liveDemoLink} className="overlay__link">
+                <GiBinoculars />
+              </a>
+            </div>
+            <h3>{project.title}</h3>
+            <div className='portfolio__buttons'>
+              <a href={project.githubLink} className="btn btn-1 btn-6 btn-6a" target="_blank"><span>Github</span> <BsGithub /></a>
+              <a href={project.liveDemoLink} className="btn btn-2 btn-6 btn-6b" target="_blank"><span>Live Demo </span><BsLink45Deg /></a>
+            </div>
+            <p>{project.description}</p>
+          </article>
+        ))}
+      </motion.div>
     </section>
-  )
+  );
 }
 
-export default Portfolio
+export default Portfolio;
